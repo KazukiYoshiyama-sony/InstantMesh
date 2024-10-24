5f3d9bf29c08285826e2844082d6d192e3d9652a
Kazuki.Yoshiyama <Kazuki.Yoshiyama@sony.com>
2024-10-24 09:56:12 +0000

    [app] share=False

--- app.py
+++ app.py
@@ -380,4 +380,4 @@ with gr.Blocks() as demo:
     )
 
 demo.queue(max_size=10)
-demo.launch(share=True, server_name="0.0.0.0", server_port=43839)
+demo.launch(share=False, server_name="0.0.0.0", server_port=43839)
